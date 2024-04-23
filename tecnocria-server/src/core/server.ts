b251c974c9cb6727c37b93614b1177e729cc57b6
Cesar <cesarvillalobosolmos.01@gmail.com>
2024-04-22 21:20:12 -0600

    Add server class

---
+++ tecnocria-server/src/core/server.ts
@@ -0,0 +1,66 @@
+import { Server as HttpServer, createServer } from 'node:http'
+import express, { Express, Router } from 'express'
+import { Server as SocketServer } from 'socket.io'
+import cors from 'cors'
+import fileUpload from 'express-fileupload'
+
+interface Options {
+  port: number
+  router: Router
+}
+
+export class Server {
+  public readonly app: Express = express()
+  public readonly server: HttpServer
+  public readonly port: number
+  public readonly router: Router
+  public readonly io: SocketServer
+
+  public constructor(options: Options) {
+    const { port, router } = options
+
+    this.port = port
+    this.router = router
+
+    this.configureMiddlewares()
+
+    this.server = createServer(this.app)
+    this.io = new SocketServer(this.server)
+
+    this.configureSockets()
+  }
+
+  public configureMiddlewares(): void {
+    // CORS
+    this.app.use(cors())
+    // JSON parser
+    this.app.use(express.json())
+    // Static folder
+    this.app.use(express.static('public'))
+    // URL parser
+    this.app.use(express.urlencoded({ extended: true }))
+    // File uploads
+    this.app.use(
+      fileUpload({
+        useTempFiles: true,
+        tempFileDir: '/tmp/',
+        createParentPath: true
+      })
+    )
+    // Router
+    this.app.use(this.router)
+  }
+
+  public configureSockets(): void {
+    this.io.on('connection', (socket) => {
+      // TODO: Use the socket controller
+      console.log('Socket connected', socket.id)
+    })
+  }
+
+  public async start(): Promise<void> {
+    this.server.listen(this.port, '0.0.0.0', () => {
+      console.log(`Server running on port ${this.port}`)
+    })
+  }
+}
