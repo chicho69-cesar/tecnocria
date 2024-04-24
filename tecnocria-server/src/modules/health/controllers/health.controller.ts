26af027e84e396577c9d0e919ae25c5a69b37916
Cesar <cesarvillalobosolmos.01@gmail.com>
2024-04-23 19:29:37 -0600

    Add changes to health controller

--- tecnocria-server/src/modules/health/controllers/health.controller.ts
+++ tecnocria-server/src/modules/health/controllers/health.controller.ts
@@ -3,11 +3,11 @@ import type { Request, Response } from 'express'
 export class HealthController {
   public constructor() {}
 
-  public healthCheck(req: Request, res: Response) {
+  healthCheck = (req: Request, res: Response) => {
     return res.send('OK')
   }
 
-  public getIsHealthy(req: Request, res: Response) {
+  getIsHealthy = (req: Request, res: Response) => {
     return res.status(200).json({
       message: 'Server is healthy'
     })
