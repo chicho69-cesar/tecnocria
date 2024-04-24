f84e06366cdb7b391e5326d7a587e03bc386c7f1
Cesar <cesarvillalobosolmos.01@gmail.com>
2024-04-23 19:33:34 -0600

    Add theme and envs configuration

---
+++ tecnocria-app/src/config/envs.ts
@@ -0,0 +1,4 @@
+export const envs = {
+  SERVER_URL: process.env.EXPO_PUBLIC_SERVER_URL,
+  SESSION_NAME: process.env.EXPO_PUBLIC_SESSION_NAME
+}
---
+++ tecnocria-app/src/config/theme.ts
@@ -0,0 +1,7 @@
+export const colors = {
+  primary: '#ED9455',
+  secondary: '#C56A2D',
+  third: '#FFC8A4',
+  active: '#25292E',
+  background: '#FFFBDA'
+}
