cf6e01022e2de76bbe2105d08352c40ac0d58a9c
Cesar <cesarvillalobosolmos.01@gmail.com>
2024-05-02 23:18:17 -0600

    Add binnacle screens

---
+++ tecnocria-app/src/modules/binnacle/screens/AddBinnacle.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native'
+
+export default function AddBinnacle() {
+  return (
+    <View>
+      <Text>AddBinnacle</Text>
+    </View>
+  )
+}
---
+++ tecnocria-app/src/modules/binnacle/screens/EditBinnacle.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from 'react-native'
+
+export default function EditBinnacle() {
+  return (
+    <View>
+      <Text>EditBinnacle</Text>
+    </View>
+  )
+}
