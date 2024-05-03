a8e615cc0113a4a173b284f3f9d2155a5eaf1f0a
Cesar <cesarvillalobosolmos.01@gmail.com>
2024-05-02 23:17:47 -0600

    Add advices screens

---
+++ tecnocria-app/src/modules/advice/screens/AdviceSection.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from 'react-native'
+
+export default function AdviceSection() {
+  return (
+    <View>
+      <Text>AdviceSection</Text>
+    </View>
+  )
+}
---
+++ tecnocria-app/src/modules/advice/screens/Advices.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from 'react-native'
+
+export default function Advices() {
+  return (
+    <View>
+      <Text>Advices</Text>
+    </View>
+  )
+}
