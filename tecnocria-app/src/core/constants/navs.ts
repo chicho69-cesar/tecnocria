7c3b1db4d0458af2505dceeb17d50f3fa019696c
Cesar <cesarvillalobosolmos.01@gmail.com>
2024-05-02 23:19:30 -0600

    Add navs constants

---
+++ tecnocria-app/src/core/constants/navs.ts
@@ -0,0 +1,41 @@
+export const STACKS = {
+  ADVICE_STACK: 'AdviceStack',
+  BINNACLE_STACK: 'BinnacleStack',
+  CHAT_STACK: 'ChatStack',
+  CONFIGURATION_STACK: 'ConfigurationStack',
+  TOOLS_STACK: 'ToolsStack'
+}
+
+export const ADVICE_SCREENS = {
+  HOME: 'Home',
+  ADVICE_SECTION: 'AdviceSection',
+  ADVICES: 'Advices',
+  ASK_FOR_ADVICES: 'AskForAdvices',
+  ABOUT: 'About',
+  TERMS_AND_CONDITIONS: 'TermsAndConditions'
+}
+
+export const BINNACLE_SCREENS = {
+  BINNACLE: 'Binnacle',
+  ADD_BINNACLE: 'AddBinnacle',
+  EDIT_BINNACLE: 'EditBinnacle'
+}
+
+export const CHAT_SCREENS = {
+  CHATS: 'Chats',
+  CHAT: 'Chat',
+  GLOBAL_CHAT: 'GlobalChat'
+}
+
+export const CONFIGURATION_SCREENS = {
+  TOOLS: 'Tools',
+  EDIT_INFO: 'EditInfo',
+  ADD_FAMILIAR: 'AddFamiliar'
+}
+
+export const TOOLS_SCREENS = {
+  SUPPORT_TOOLS: 'SupportTools',
+  BOOKS: 'Books',
+  AUDIOVISUAL: 'Audiovisual',
+  PROFESSIONALS: 'Professionals'
+}
