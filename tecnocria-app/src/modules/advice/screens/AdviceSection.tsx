5a2682010c2bfcbf298d6dee0df4ad12ef472b4d
Cesar <cesarvillalobosolmos.01@gmail.com>
2024-05-04 23:50:50 -0600

    Add advice section screen

--- tecnocria-app/src/modules/advice/screens/AdviceSection.tsx
+++ tecnocria-app/src/modules/advice/screens/AdviceSection.tsx
@@ -1,12 +1,93 @@
-import { Container } from '@/core/ui'
-import { Text, View } from 'react-native'
+import { StyleSheet, View } from 'react-native'
+
+import { ADVICE_SCREENS, STACKS, useNavigate } from '@/core'
+import { Container, InfoCard, Title } from '@/core/ui'
+import { AdviceCategory } from '../components'
+
+const maternity = require('../../../../assets/categories/maternidad.png')
+const waysOfParenting = require('../../../../assets/categories/formas-de-crianza.png')
+const didacticGames = require('../../../../assets/categories/juegos-didacticos.png')
+const securityAndHealth = require('../../../../assets/categories/salud.png')
+const education = require('../../../../assets/categories/educacion.png')
+const workAndFamilyLife = require('../../../../assets/categories/vida.png')
+const nutrition = require('../../../../assets/categories/nutricion.png')
+const motorAndCognitive = require('../../../../assets/categories/desarrollo.png')
 
 export default function AdviceSection() {
+  const { navigateBetweenRoutes } = useNavigate()
+
+  const handlePress = (category: string) => {
+    navigateBetweenRoutes(STACKS.ADVICE_STACK, ADVICE_SCREENS.ADVICES, {
+      category
+    })
+  }
+
   return (
     <Container>
-      <View>
-        <Text>AdviceSection</Text>
+      <InfoCard
+        title='Consejos'
+        description='Explora diversas categorías de consejos para apoyarte en la crianza de tus hijos.'
+      />
+
+      <Title title='Categorías' />
+
+      <View style={styles.grid}>
+        <AdviceCategory
+          title='Maternidad'
+          image={maternity}
+          onPress={() => handlePress('Maternidad')}
+        />
+
+        <AdviceCategory
+          title='Formas de crianza'
+          image={waysOfParenting}
+          onPress={() => handlePress('Formas de crianza')}
+        />
+
+        <AdviceCategory
+          title='Juegos didácticos'
+          image={didacticGames}
+          onPress={() => handlePress('Juegos didácticos')}
+        />
+
+        <AdviceCategory
+          title='Salud y seguridad'
+          image={securityAndHealth}
+          onPress={() => handlePress('Salud y seguridad')}
+        />
+
+        <AdviceCategory
+          title='Educación'
+          image={education}
+          onPress={() => handlePress('Educación')}
+        />
+
+        <AdviceCategory
+          title='Vida laboral y familiar'
+          image={workAndFamilyLife}
+          onPress={() => handlePress('Vida laboral y familiar')}
+        />
+
+        <AdviceCategory
+          title='Nutrición'
+          image={nutrition}
+          onPress={() => handlePress('Nutrición')}
+        />
+
+        <AdviceCategory
+          title='Desarrollo motriz y cognitivo'
+          image={motorAndCognitive}
+          onPress={() => handlePress('Desarrollo motriz y cognitivo')}
+        />
       </View>
     </Container>
   )
 }
+
+const styles = StyleSheet.create({
+  grid: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'space-between'
+  }
+})
--- tecnocria-app/src/modules/advice/screens/Advices.tsx
+++ tecnocria-app/src/modules/advice/screens/Advices.tsx
@@ -1,7 +1,11 @@
+import { useParams } from '@/core'
 import { Container } from '@/core/ui'
 import { Text, View } from 'react-native'
 
 export default function Advices() {
+  const { category } = useParams<{ category: string }>()
+  console.log(category)
+
   return (
     <Container>
       <View>
