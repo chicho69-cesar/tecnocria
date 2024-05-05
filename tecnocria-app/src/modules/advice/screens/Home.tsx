8edac7ad89977d746ae86da6ea2a1eda427fa054
Cesar <cesarvillalobosolmos.01@gmail.com>
2024-05-04 23:50:39 -0600

    Add home screen

--- tecnocria-app/src/modules/advice/screens/Home.tsx
+++ tecnocria-app/src/modules/advice/screens/Home.tsx
@@ -1,14 +1,47 @@
-import { Container, Logo } from '@/core/ui'
-import { Text, View } from 'react-native'
+import { ADVICE_SCREENS, STACKS, TOOLS_SCREENS, useNavigate } from '@/core'
+import { Container, InfoCard, Logo, Title } from '@/core/ui'
+import { AdviceCard } from '../components'
+
+const advices = require('../../../../assets/img/advices.png')
+const kids = require('../../../../assets/img/kids.png')
 
 export default function Home() {
+  const { navigateBetweenRoutes } = useNavigate()
+
   return (
     <Container>
       <Logo />
 
-      <View>
-        <Text>Home</Text>
-      </View>
+      <InfoCard
+        title='Bienvenido'
+        description='Recuerda ser el adulto que necesitabas cuando eras niño.'
+      />
+
+      <Title title='Recomendaciones' />
+
+      <AdviceCard
+        image={advices}
+        title='Explora la sección de consejos'
+        onPress={() =>
+          navigateBetweenRoutes(
+            STACKS.ADVICE_STACK,
+            ADVICE_SCREENS.ADVICE_SECTION,
+            {}
+          )
+        }
+      />
+
+      <AdviceCard
+        image={kids}
+        title='Explora las herramientas de apoyo'
+        onPress={() =>
+          navigateBetweenRoutes(
+            STACKS.TOOLS_STACK,
+            TOOLS_SCREENS.SUPPORT_TOOLS,
+            {}
+          )
+        }
+      />
     </Container>
   )
 }
