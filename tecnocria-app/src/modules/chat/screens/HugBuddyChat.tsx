200bf5e46c07169addbb9fb7d3bf7db7cffcb6bf
Cesar <cesarvillalobosolmos.01@gmail.com>
2024-05-04 23:49:43 -0600

    Add init of new planned screens

---
+++ tecnocria-app/src/modules/chat/screens/HugBuddyChat.tsx
@@ -0,0 +1,12 @@
+import { Container } from '@/core/ui'
+import { Text, View } from 'react-native'
+
+export default function HugBuddyChat() {
+  return (
+    <Container>
+      <View>
+        <Text>Chat</Text>
+      </View>
+    </Container>
+  )
+}
--- tecnocria-app/src/modules/chat/screens/index.ts
+++ tecnocria-app/src/modules/chat/screens/index.ts
@@ -1,3 +1,5 @@
 export { default as Chat } from './Chat'
 export { default as Chats } from './Chats'
+export { default as CreateChat } from './CreateChat'
 export { default as GlobalChat } from './GlobalChat'
+export { default as HugBuddyChat } from './HugBuddyChat'
