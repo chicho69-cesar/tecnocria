1b68ae10a7926e17c7d34f3cdbac0b1300f8211f
Cesar <cesarvillalobosolmos.01@gmail.com>
2024-05-07 20:54:35 -0600

    Update router with all app routes

--- tecnocria-server/src/core/router.ts
+++ tecnocria-server/src/core/router.ts
@@ -1,13 +1,27 @@
 import { Router } from 'express'
-import { HealthRoutes } from '../modules/health'
+import { AdviceRoutes } from '../modules/advices'
+import { AIRoutes } from '../modules/ai'
 import { AuthRoutes } from '../modules/auth'
+import { BinnacleRoutes } from '../modules/binnacle'
+import { ChatRoutes } from '../modules/chat'
+import { HealthRoutes } from '../modules/health'
+import { ParentsRoutes } from '../modules/parents'
+import { ResourcesRoutes } from '../modules/resources'
+import { UsersRoutes } from '../modules/users'
 
 export class AppRouter {
   static get routes(): Router {
     const router = Router()
 
-    router.use('/api/health', HealthRoutes.routes)
+    router.use('/api/advices', AdviceRoutes.routes)
+    router.use('/api/ai', AIRoutes.routes)
     router.use('/api/auth', AuthRoutes.routes)
+    router.use('/api/binnacles', BinnacleRoutes.routes)
+    router.use('/api/chats', ChatRoutes.routes)
+    router.use('/api/health', HealthRoutes.routes)
+    router.use('/api/parents', ParentsRoutes.routes)
+    router.use('/api/resources', ResourcesRoutes.routes)
+    router.use('/api/users', UsersRoutes.routes)
 
     return router
   }
