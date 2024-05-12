debabb85c55e35e08c6e96bd84fd35a0806180e5
Cesar <cesarvillalobosolmos.01@gmail.com>
2024-05-11 19:11:49 -0600

    Add tools service

---
+++ tecnocria-app/src/modules/tools/services/tools.service.ts
@@ -0,0 +1,60 @@
+import axios, { AxiosError } from 'axios'
+
+import { envs } from '@/config'
+import { CustomError } from '@/core'
+
+export async function getBooksAI(token: string) {
+  try {
+    const { data, status } = await axios.get<{ data: string }>(
+      `${envs.SERVER_URL}/resources/books`,
+      {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      }
+    )
+
+    if (status === 401) {
+      throw CustomError.unauthorized('Unauthorized')
+    }
+
+    return data
+  } catch (error) {
+    console.log(`Error en el servicio get books AI: ${error}`)
+
+    if (error instanceof AxiosError) {
+      const { response } = error
+      throw new CustomError(response!.status, response!.data.error)
+    }
+
+    throw CustomError.internalServer('Internal Server Error')
+  }
+}
+
+export async function getAudiovisualAI(token: string) {
+  try {
+    const { data, status } = await axios.get<{ data: string }>(
+      `${envs.SERVER_URL}/resources/audiovisual`,
+      {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      }
+    )
+
+    if (status === 401) {
+      throw CustomError.unauthorized('Unauthorized')
+    }
+
+    return data
+  } catch (error) {
+    console.log(`Error en el servicio get audiovisual AI: ${error}`)
+
+    if (error instanceof AxiosError) {
+      const { response } = error
+      throw new CustomError(response!.status, response!.data.error)
+    }
+
+    throw CustomError.internalServer('Internal Server Error')
+  }
+}
