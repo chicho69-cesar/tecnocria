5c3072c9d77a47e1af1c41f58626d944229cb8b3
Cesar <cesarvillalobosolmos.01@gmail.com>
2024-05-11 19:10:19 -0600

    Add advice service

---
+++ tecnocria-app/src/modules/advice/services/advice.service.ts
@@ -0,0 +1,66 @@
+import axios, { AxiosError } from 'axios'
+
+import { envs } from '@/config'
+import { CustomError } from '@/core'
+
+export async function getAdvices(title: string, token: string) {
+  try {
+    const { data, status } = await axios.post<{ data: string }>(
+      `${envs.SERVER_URL}/advices`,
+      {
+        title
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      }
+    )
+
+    if (status === 401) {
+      throw CustomError.unauthorized('Unauthorized')
+    }
+
+    return data
+  } catch (error) {
+    console.log(`Error en el servicio get advices: ${error}`)
+
+    if (error instanceof AxiosError) {
+      const { response } = error
+      throw new CustomError(response!.status, response!.data.error)
+    }
+
+    throw CustomError.internalServer('Internal Server Error')
+  }
+}
+
+export async function giveMeAdvices(message: string, token: string) {
+  try {
+    const { data, status } = await axios.post<{ data: string }>(
+      `${envs.SERVER_URL}/advices/give-me`,
+      {
+        message
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      }
+    )
+
+    if (status === 401) {
+      throw CustomError.unauthorized('Unauthorized')
+    }
+
+    return data
+  } catch (error) {
+    console.log(`Error en el servicio give me advices: ${error}`)
+
+    if (error instanceof AxiosError) {
+      const { response } = error
+      throw new CustomError(response!.status, response!.data.error)
+    }
+
+    throw CustomError.internalServer('Internal Server Error')
+  }
+}
