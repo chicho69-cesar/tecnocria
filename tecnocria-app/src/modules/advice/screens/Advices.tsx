84502a9db4fab0f32d66dafced30813aa7a520a9
Cesar <cesarvillalobosolmos.01@gmail.com>
2024-05-17 23:00:21 -0600

    Add advices route screen

--- tecnocria-app/src/modules/advice/screens/Advices.tsx
+++ tecnocria-app/src/modules/advice/screens/Advices.tsx
@@ -1,16 +1,71 @@
-import { useParams } from '@/core'
-import { Container } from '@/core/ui'
-import { Text, View } from 'react-native'
+import { colors } from '@/config'
+import { ADVICE_SCREENS, STACKS, useNavigate, useParams } from '@/core'
+import { Button, Container, InfoCard, Title } from '@/core/ui'
+import { ActivityIndicator, StyleSheet, Text, View } from 'react-native'
+import { useAdvices } from '../hooks'
 
 export default function Advices() {
+  const { navigateBetweenRoutes } = useNavigate()
   const { category } = useParams<{ category: string }>()
-  console.log(category)
+  const { loading, advices } = useAdvices(category)
 
   return (
     <Container>
+      <InfoCard
+        title={`Consejos sobre ${category}`}
+        description={`Explora los consejos y recomendaciones sobre ${category}`}
+      />
+
+      <Title title='Consejos' />
+
       <View>
-        <Text>Advices</Text>
+        {loading ? (
+          <View style={{ marginVertical: 16 }}>
+            <ActivityIndicator size='large' color={colors.primary} />
+          </View>
+        ) : (
+          <View style={{ marginVertical: 12 }}>
+            <View style={styles.container}>
+              <Text style={styles.text}>{advices.data}</Text>
+            </View>
+
+            <Button
+              onPress={() => {
+                navigateBetweenRoutes(
+                  STACKS.ADVICE_STACK,
+                  ADVICE_SCREENS.ASK_FOR_ADVICES,
+                  {}
+                )
+              }}>
+              <Text style={styles.buttonText}>
+                Pide consejos personalizados
+              </Text>
+            </Button>
+          </View>
+        )}
       </View>
     </Container>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: 8,
+    marginBottom: 16,
+    borderRadius: 8,
+    backgroundColor: colors.fourth,
+    padding: 16,
+    elevation: 2
+  },
+  text: {
+    fontSize: 17,
+    color: '#25292e',
+    fontFamily: 'Roboto'
+  },
+  buttonText: {
+    fontSize: 18,
+    fontFamily: 'Acme',
+    color: '#fff'
+  }
+})
