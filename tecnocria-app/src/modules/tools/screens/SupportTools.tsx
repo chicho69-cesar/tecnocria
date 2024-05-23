289ce0ca1920e8bd445ffe664a459c62103ad073
Cesar <cesarvillalobosolmos.01@gmail.com>
2024-05-23 00:00:05 -0600

    Add suport tools resources

--- tecnocria-app/src/modules/tools/screens/SupportTools.tsx
+++ tecnocria-app/src/modules/tools/screens/SupportTools.tsx
@@ -1,12 +1,80 @@
-import { Container } from '@/core/ui'
-import { Text, View } from 'react-native'
+import { colors } from '@/config'
+import { STACKS, TOOLS_SCREENS, useNavigate } from '@/core'
+import { Container, InfoCard, Logo } from '@/core/ui'
+import { Image, StyleSheet, Text, TouchableOpacity } from 'react-native'
+
+const books = require('../../../../assets/img/bitacora.png')
+const audiovisual = require('../../../../assets/img/herramientas.png')
+// const professionals = require('../../../../assets/img/rompe-cabezas.png')
 
 export default function SupportTools() {
+  const { navigateBetweenRoutes } = useNavigate()
+
   return (
     <Container>
-      <View>
-        <Text>SupportTools</Text>
-      </View>
+      <Logo />
+
+      <InfoCard
+        title='Herramientas de apoyo'
+        description='Aquí encontrarás herramientas de apoyo para tu día a día.'
+      />
+
+      <TouchableOpacity
+        style={styles.card}
+        onPress={() => {
+          navigateBetweenRoutes(STACKS.TOOLS_STACK, TOOLS_SCREENS.BOOKS, {})
+        }}>
+        <Text style={styles.title}>Libros</Text>
+        <Image source={books} style={styles.image} />
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        style={styles.card}
+        onPress={() => {
+          navigateBetweenRoutes(
+            STACKS.TOOLS_STACK,
+            TOOLS_SCREENS.AUDIOVISUAL,
+            {}
+          )
+        }}>
+        <Text style={styles.title}>Recursos Audiovisuales</Text>
+        <Image source={audiovisual} style={styles.image} />
+      </TouchableOpacity>
+
+      {/* <TouchableOpacity
+        style={styles.card}
+        onPress={() => {
+          navigateBetweenRoutes(
+            STACKS.TOOLS_STACK,
+            TOOLS_SCREENS.PROFESSIONALS,
+            {}
+          )
+        }}>
+        <Text style={styles.title}>Profesionales</Text>
+        <Image source={professionals} style={styles.image} />
+      </TouchableOpacity> */}
     </Container>
   )
 }
+
+const styles = StyleSheet.create({
+  card: {
+    backgroundColor: colors.fourth,
+    borderRadius: 10,
+    padding: 20,
+    marginVertical: 10,
+    elevation: 2,
+    justifyContent: 'center',
+    alignItems: 'center',
+    gap: 8
+  },
+  title: {
+    fontSize: 20,
+    color: '#25292e',
+    fontFamily: 'Acme'
+  },
+  image: {
+    width: 160,
+    height: 160
+  }
+})
